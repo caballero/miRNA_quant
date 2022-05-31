4720e1e9e3ba1596ccd132c45dacbf6835a1f99d
Juan Caballero <jcaballero@ebi.ac.uk>
2022-05-31 10:53:17 -0500

    script to dedup a fasta file based on seqs

--- dedupFasta.py
+++ dedupFasta.py
@@ -15,7 +15,7 @@ prefix   = ">seq_"
 
 seqs = {}
 print("Reading sequences from {}".format(in_file))
-if (in_file.endsWith(".gz")):
+if (in_file.endswith(".gz")):
     fi = gzip.open(in_file, 'rt')
 else:
     fi = open(in_file, 'r')
@@ -32,7 +32,7 @@ for line in fi:
 fi.close()
 
 print("writing output to {}".format(out_file))
-if (out_file.endsWith(".gz")):
+if (out_file.endswith(".gz")):
     fo = gzip.open(out_file, 'wt')
 else:
     fo = open(out_file, 'w')
